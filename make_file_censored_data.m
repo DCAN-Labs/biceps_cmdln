47c805685583657fada22b1c0bd440d3af6c3216
Kody DeGolier <baron063@acn01.agate.msi.umn.edu>
2025-06-26 09:18:24 -0500

    Enhanced logic to handle singe subject cases

--- make_file_censored_data.m
+++ make_file_censored_data.m
@@ -6,8 +6,16 @@ n=sum(ix,1);
 fileID = fopen(surv_file,'wt');
 
 ix1=find(ix(:,1));
-T=table(handles.participants.full_path(ix1,:));
+if n(1) == 1
+    % One subject: wrap single char row in cell array
+    T = table({handles.participants.full_path(ix1,:)});
+else
+    % Multiple subjects: char array becomes cell array of rows
+    T = table(cellstr(handles.participants.full_path(ix1,:)));
+end
+% T=table(handles.participants.full_path(ix1,:));
 writetable(T,surv_file,'WriteVariableNames',0);
+
 % for i=1:n(1)
 %     fprintf(fileID,[handles.participants.full_path(ix1(i),:) '\n']);
 % end
